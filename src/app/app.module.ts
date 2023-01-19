3cecbca5874207f49bf82f03c513a26043f028c5
tib.songsakul <osacn_sataporn.songsakul@krungthai.com>
2023-01-19 17:18:18 +0700

    add test routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgxExtendedPdfViewerModule } from 'ngx-extended-pdf-viewer';
+import { TestComponent } from './test/test.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, TestComponent],
   imports: [BrowserModule, AppRoutingModule, NgxExtendedPdfViewerModule],
   providers: [],
   bootstrap: [AppComponent],
