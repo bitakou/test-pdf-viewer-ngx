03cf89624f018bd436d03a33c22a9995ab0171eb
tib.songsakul <osacn_sataporn.songsakul@krungthai.com>
2023-01-20 15:10:36 +0700

    try adding title

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,9 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { TestComponent } from './test/test.component';
 
-const routes: Routes = [{ path: 'test', component: TestComponent }];
+const routes: Routes = [
+  { path: 'test', title: 'this is test title', component: TestComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
